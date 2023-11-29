d486da050ede9b1c23ed7db456a231dc7bca2b80
AtomicPsythe <fotibuddy@gmail.com>
2023-11-29 17:11:35 -0500

    last minute updates

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-let ahri = new Audio("links-and-images/sound/ahri-sound.mp3");
+function onButtonClick() {
+
+  }
+  
+  const button = document.querySelector('button');
+  button.addEventListener('click', onButtonClick);
+
+  const obj = document.createElement("audio");
+  console.log(obj.volume); // 1
+  obj.volume = 0.75;
